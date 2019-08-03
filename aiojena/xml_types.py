1772d121774bf3c49e29a63049b946775ffc1da0
Adam Bannister <adam.p.bannister@gmail.com>
2019-08-03 09:47:23 +0200

    add xml types

---
+++ aiojena/xml_types.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name: types.py
+
+
+string_type = '"^^<http://www.w3.org/2001/XMLSchema:string>'
+integer_type = '"^^<http://www.w3.org/2001/XMLSchema:integer>'
