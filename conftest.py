7569ce669cb8dd15df565fbfaa6ed9765a8f09bc
Adam Bannister <adam.p.bannister@gmail.com>
2019-08-17 17:08:08 +0200

    move helper functions to conftest

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # File name: conftest.py
 
+from typing import List
 import pytest
 from aiojena.client import JenaClient
 
@@ -22,3 +23,56 @@ async def truncate_jena(jena_client: JenaClient):
         }
         """
     )
+
+
+@pytest.fixture
+async def simple_insert():
+    async def _simple_insert(jena_client, subj, pred, obj) -> None:
+        await jena_client.update(
+            """
+            INSERT DATA {{
+            {subj}     {pred}      {obj}
+            }}
+            """, {
+                'subj': subj,
+                'pred': pred,
+                'obj': obj
+            }
+        )
+    return _simple_insert
+
+
+@pytest.fixture
+async def simple_select_predicate():
+    async def _simple_select_predicate(jena_client, subj, pred, obj) -> List:
+        return await jena_client.query(
+            """
+            SELECT {pred}
+            WHERE {{
+            {subj}     {pred}      {obj}
+            }}
+            """, {
+                'subj': subj,
+                'pred': pred,
+                'obj': obj
+            }
+        )
+    return _simple_select_predicate
+
+
+@pytest.fixture
+async def simple_select_object():
+    async def _simple_select_object(jena_client, subj, pred, obj) -> List:
+        return await jena_client.query(
+            """
+            SELECT {obj}
+            WHERE {{
+            {subj}     {pred}      {obj}
+            }}
+            """, {
+                'subj': subj,
+                'pred': pred,
+                'obj': obj
+            }
+        )
+    return _simple_select_object
