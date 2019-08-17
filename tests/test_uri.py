b14cae4145c30fb9ce272edf1abf12cc7ecf680b
Adam Bannister <adam.p.bannister@gmail.com>
2019-08-17 17:08:49 +0200

    update uri tests to use URIRef from rdflib

--- tests/test_uri.py
+++ tests/test_uri.py
@@ -3,33 +3,27 @@
 # File name: test_uri.py
 
 import pytest
-from aiojena.uri import URI, parse_uri_value
+from rdflib import URIRef
 
 
 @pytest.mark.asyncio
-async def test_uri(jena_client, truncate_jena):
-    test_uri_string = 'rdfs:label'
-    test_uri = URI(test_uri_string)
-    insert_query = """
-    INSERT DATA {{
-      <http://test.com>     {test_uri}      'Label'
-    }}
-    """.format(test_uri=test_uri)
-    await jena_client.update(insert_query)
-
-    select_query = """
-    SELECT ?uri
-    WHERE {
-      <http://test.com>     ?uri            'Label'
-    }
-    """
-    results = await jena_client.query(select_query)
+async def test_uri(
+    jena_client, truncate_jena, simple_insert, simple_select_predicate
+):
+    test_subject = URIRef('http://test.com')
+    test_predicate = URIRef('http://www.w3.org/2000/01/rdf-schema#:label')
+    test_object = 'Label'
+    await simple_insert(
+        jena_client=jena_client,
+        subj=test_subject,
+        pred=test_predicate,
+        obj=test_object,
+    )
+    results = await simple_select_predicate(
+        jena_client=jena_client,
+        subj=test_subject,
+        pred='?uri',
+        obj=test_object,
+    )
     assert len(results) == 1
-    assert results[0]['uri'] == test_uri
-
-
-def test_parse_uri_value():
-    rdfs_label = 'rdfs:label'
-    rdfs_label_url = '<http://www.w3.org/2000/01/rdf-schema#label>'
-    assert parse_uri_value(value=rdfs_label) == rdfs_label_url
-    assert parse_uri_value(value=rdfs_label_url) == rdfs_label_url
+    assert results[0]['uri'] == test_predicate.n3()
