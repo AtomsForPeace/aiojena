576e8c0a207546ba3964068d52fec9c6491030f8
Adam Bannister <adam.p.bannister@gmail.com>
2019-08-19 19:36:23 +0200

    add test for values given in tuple form

--- tests/test_literal.py
+++ tests/test_literal.py
@@ -53,3 +53,35 @@ async def test_int_literal(
     )
     assert len(results) == 1
     assert results[0]['integer'] == test_object
+
+
+@pytest.mark.asyncio
+async def test_int_literal_iter(
+    jena_client, truncate_jena, simple_insert, simple_select_object
+):
+    test_subject = URIRef('http://test.com')
+    test_predicate = URIRef('http://www.w3.org/2000/01/rdf-schema#:label')
+    test_object = 1
+
+    await simple_insert(
+        jena_client=jena_client,
+        subj=test_subject,
+        pred=test_predicate,
+        obj=test_object
+    )
+
+    results = await jena_client.query(
+        """
+        SELECT ?integer
+        WHERE {{
+          {subj} {pred} ?integer .
+          FILTER(?integer IN {test_objects})
+        }}
+        """, {
+            'subj': test_subject,
+            'pred': test_predicate,
+            'test_objects': tuple([test_object, ])
+        }
+    )
+    assert len(results) == 1
+    assert results[0]['integer'] == test_object
