d7e8828269e49f3213fcce4fadb6afda80696313
Adam Bannister <adam.p.bannister@gmail.com>
2019-09-29 00:02:59 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='aiojena',
-    version='0.0.1',
+    version='0.0.2',
     description='A Jena wrapper for aiosparql with automatic type conversion',
     install_requires=['RDFLib', ],
 )
