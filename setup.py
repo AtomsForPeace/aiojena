0f4497ea697dd33ee0a45ef06262f096b8813e39
Adam Bannister <adam.p.bannister@gmail.com>
2019-09-30 22:07:37 +0200

    bump version, have tests working with docker now

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='aiojena',
-    version='0.0.2',
+    version='0.0.3',
     description='A Jena wrapper for aiosparql with automatic type conversion',
     install_requires=['RDFLib', ],
 )
