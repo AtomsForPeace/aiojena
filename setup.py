1682afe36f9748494afb2815114c03aab4640ad7
Adam Bannister <adam.p.bannister@gmail.com>
2019-10-21 21:24:10 +0200

    update version to 0.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='aiojena',
-    version='0.0.3',
+    version='0.0.4',
     packages=['aiojena']
     description='A Jena wrapper for aiosparql with automatic type conversion',
     install_requires=['RDFLib', ],
