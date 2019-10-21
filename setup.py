5aa1c3b8cc9d98d6e7b16d239cac535803527b26
Adam Bannister <adam.p.bannister@gmail.com>
2019-10-21 21:26:24 +0200

    bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='aiojena',
-    version='0.0.4',
+    version='0.0.5',
     packages=['aiojena'],
     description='A Jena wrapper for aiosparql with automatic type conversion',
     install_requires=['RDFLib', 'aiosparql'],
